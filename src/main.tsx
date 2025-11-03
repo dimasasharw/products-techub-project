3b2e55ac9e7405d0624fb13110a5ff6f56825d8a
Dimas Ashar <dimas.ashar1997@gmail.com>
2025-11-03 20:25:53 +0700

    routes installation

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import App from "./App.tsx";
+import router from "./routes/index.tsx";
 
 import "./index.css";
 import "flowbite";
 
-createRoot(document.getElementById("root")!).render(
+// import App from "./App.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
