4775399b37841bf04b97f1412dd40e89b3393a5c
dimasasharw <dimas.ashar1997@gmail.com>
2025-11-04 18:44:50 +0700

    Edit Components

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 export default function BaseLayout() {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-900">
-      <nav className="p-4 bg-white shadow sticky top-0 flex gap-4">
+      <nav className="p-4 bg-white shadow sticky top-0 flex gap-4 z-10">
         <Link to="/" className="text-blue-600 hover:underline">
           Products
         </Link>
