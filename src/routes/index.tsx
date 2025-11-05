c74a2521f67ed34f3f6b4c2a5e375d25f1eaf68f
Dimas Ashar <dimas.ashar1997@gmail.com>
2025-11-06 06:52:19 +0700

    indexing import

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,17 @@
 import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
-const BaseLayout = lazy(() => import("../components/BaseLayout"));
-const ProductListPage = lazy(() => import("../views/ProductListPage"));
-const BrandPage = lazy(() => import("../views/BrandPage"));
-const ProductDetailPage = lazy(() => import("../views/ProductDetailPage"));
+const BaseLayout = lazy(() => import("@/components/BaseLayout"));
+const BrandPage = lazy(() => import("@/views/BrandPage"));
+const ProductListPage = lazy(() => import("@/views/ProductListPage"));
+const ProductDetailPage = lazy(() => import("@/views/ProductDetailPage"));
 
 const router = createBrowserRouter([
   {
     element: <BaseLayout />,
     children: [
-      { path: "/", element: <ProductListPage /> },
+      { path: "/", element: <BrandPage /> },
+      { path: "/all-products", element: <ProductListPage /> },
       { path: "/brands", element: <BrandPage /> },
       { path: "/brands/:brand", element: <ProductListPage /> },
       { path: "/categories/:category", element: <ProductListPage /> },
